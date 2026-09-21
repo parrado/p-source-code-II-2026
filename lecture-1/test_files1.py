ed6a408cac17b0276e69677ccc114cfe8a128526
Alexander López-Parrado <parrado@uniquindio.edu.co>
2026-09-21 08:47:19 -0500

    Add files via upload

---
+++ lecture-1/test_files1.py
@@ -0,0 +1,17 @@
+from json import dumps
+
+# Abre archivo
+file=open('data/my_data.txt','a')
+#file=open('C:/Users/samae/Documents/docencia-uq/II-2025/Programming/slides/lecture-1-sources/data/my_data.txt','a')
+# Solicita datos al usuario
+name=input('Ingrese su nombre: ')
+id=int(input('Ingrese su cédula: '))
+
+data={"name":name,"id":id}
+
+# Escribe el texto en el archivo
+file.write(dumps(data)+"\n")
+
+# Cierra el archivo
+file.close()
+
